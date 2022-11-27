380e824704e273bd86a044118d9a0e8e5c052077
Ash <119080371+n9ash@users.noreply.github.com>
2022-11-27 09:44:51 +0100

    initial commit

---
+++ PetBattlePerformance/PetBattlePerformance.lua
@@ -0,0 +1,49 @@
+local AddonName, PetBattlePerformance = ...
+local AddonTitle = GetAddOnMetadata(AddonName, "Title") or AddonName;
+local AddonVersion = GetAddOnMetadata(AddonName, "Version") or "?";
+
+--------------------------------------------------------------------------------
+-- Frame Scripts
+--------------------------------------------------------------------------------
+
+local frame = CreateFrame("Frame");
+
+function frame:OnEvent(event, ...)
+	if (event == "ADDON_LOADED") then
+		local arg1 = select(1, ...);
+		if (arg1 == AddonName) then
+			DEFAULT_CHAT_FRAME:AddMessage("[" .. AddonTitle .. "] |cFF00FF00" .. AddonVersion .. "|r loaded.", 0.7, 0.7, 1.0);
+
+			frame:RegisterEvent("PET_BATTLE_OPENING_DONE");
+		end
+	elseif (event == "PET_BATTLE_OPENING_DONE") then
+		frame:RegisterEvent("PET_BATTLE_PET_ROUND_PLAYBACK_COMPLETE");
+		frame:RegisterEvent("PET_BATTLE_FINAL_ROUND");
+		frame:RegisterEvent("PET_BATTLE_CLOSE");
+		frame.startTime = GetTime();
+	elseif (event == "PET_BATTLE_PET_ROUND_PLAYBACK_COMPLETE") then
+		frame.round = select(1, ...);
+	elseif (event == "PET_BATTLE_FINAL_ROUND") then
+		frame.endTime = GetTime();
+		frame.winner = select(1, ...);
+	elseif (event == "PET_BATTLE_CLOSE") then
+		frame:UnregisterEvent("PET_BATTLE_PET_ROUND_PLAYBACK_COMPLETE");
+		frame:UnregisterEvent("PET_BATTLE_FINAL_ROUND");
+		frame:UnregisterEvent("PET_BATTLE_CLOSE");
+
+		local duration = ceil(frame.endTime - frame.startTime);
+		local minutes, seconds = floor(duration / 60), duration % 60;
+
+		local d = YELLOW_FONT_COLOR_CODE .. (minutes > 0 and format("%dm %ds", minutes, seconds) or format("%ds", seconds)) .. "|r";
+		local r = YELLOW_FONT_COLOR_CODE .. frame.round .. "|r";
+		local o = YELLOW_FONT_COLOR_CODE .. (frame.winner == 1 and "Win" or "Loss") .. "|r";
+
+		DEFAULT_CHAT_FRAME:AddMessage("[" .. AddonTitle .. "] This battle lasted " .. d ..
+			" over " .. r .. " rounds, and resulted in a " .. o .. ".", 0.7, 0.7, 1.0);
+	else
+		print("[OnEvent] " .. event .. " NYI");
+	end
+end
+
+frame:RegisterEvent("ADDON_LOADED");
+frame:SetScript("OnEvent", frame.OnEvent);
---
+++ README.md
@@ -0,0 +1,9 @@
+<div align="center">
+
+# Pet Battle Performance
+
+![Screenshot](assets/PetBattlePerformance.jpg)
+
+</div>
+
+A minimalistic World of Warcraft addon that prints a pet battle performance after each pet battle.
